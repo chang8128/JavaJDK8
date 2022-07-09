d5309acd6a6221404f5d7fc8706079ec41e1fe5f
chang8128 <chang8128@sina.cn>
2022-07-09 18:14:04 +0800

    9*9table

--- CH03Basic/Basic/src/cc/openhome/OneToTen.java
+++ CH03Basic/Basic/src/cc/openhome/OneToTen.java
@@ -2,6 +2,11 @@ package cc.openhome;
 
 public class OneToTen {
     public static void main(String[] args) {
+
+        int a = 28;
+        float b = 13.010101f;
+        System.out.printf("整数是：%d,%n小数是：%f",a,b);
+
         for(int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
