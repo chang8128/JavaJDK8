d84947b6e46c253cc68a0b42bb4950c32917f5b3
chang8128 <chang8128@sina.cn>
2022-07-10 11:08:10 +0800

    delete wrong code

--- Begin/src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        float radius = 88.2f;
-        double area = 2 * 3.14 * radius * radius;
-        System.out.println(area);
-    }
-}
