ffc80c930c5c677559791a0946b2b7b07e06d997
chang8128 <chang8128@sina.cn>
2022-07-11 12:49:00 +0800

    第四章

---
+++ CH04/src/Sum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long sum = 0;
+        long number = 0;
+        do {
+            System.out.print("输入数字： ");
+            number = Long.parseLong(scanner.nextLine());
+            sum += number;
+        } while(number != 0);
+        System.out.println("总和：" +sum);
+    }
+
+}
