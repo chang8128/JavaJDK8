c24694b4cbe80b8968d22ed1fc233824b9babd8d
chang8128 <chang8128@sina.cn>
2022-07-13 20:27:50 +0800

    revise shuffle of Collections class

--- CH04/src/ShuffleDemo.java
+++ CH04/src/ShuffleDemo.java
@@ -9,19 +9,19 @@ public class ShuffleDemo {
     // static void shuffle(List<?>list, Random rand)使用指定的随机源对指定列表进行置换
     public static void main(String[] args) {
         Random rand = new Random();
-        String[] ia = {"A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        String[] ranks = {"A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
         // 第一种方法
-        List<String> list = new ArrayList<String>(Arrays.asList(ia));
+        List<String> list = new ArrayList<String>(Arrays.asList(ranks));
         System.out.println("前 shuffling: " + list);
         Collections.shuffle(list,rand);
         System.out.println("后 shuffling: " + list);
                      
         //第二种方法
         System.out.println("第二种方法：数组化");
-        List<String> list1 = Arrays.asList(ia);
+        List<String> list1 = Arrays.asList(ranks);
         System.out.println("前 shuffling: " + list1);
         Collections.shuffle(list1,rand);
-        System.out.println("后 shuffling: " + Arrays.toString(ia));
+        System.out.println("后 shuffling: " + Arrays.toString(ranks));
     }
 }
